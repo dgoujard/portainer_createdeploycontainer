e56f715ffbc5ffef97dec9e512bfe59d199219ab
Damien Goujard <damien@macuser.fr>
2020-01-26 15:42:37 +0100

    add action

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const got = require('got');
 const uuidv1 = require('uuid/v1');
+const core = require('@actions/core');
 
 (async () => {
 
-    const LOGIN = process.env.LOGIN || "admin";
-    const PASSWORD = process.env.PASSWORD;
-    const URL = process.env.PASSWORD;
-    const CONTAINER_IMAGE = process.env.CONTAINER_IMAGE;
-    const CONTAINER_HOST_MOUNT_VOLUME = process.env.CONTAINER_HOST_MOUNT_VOLUME;
+    const LOGIN = process.env.LOGIN || core.getInput('login') || "admin";
+    const PASSWORD = process.env.PASSWORD || core.getInput('password');
+    const URL = process.env.URL || core.getInput('url');
+    const CONTAINER_IMAGE = process.env.CONTAINER_IMAGE || core.getInput('container_image');
+    const CONTAINER_HOST_MOUNT_VOLUME = process.env.CONTAINER_HOST_MOUNT_VOLUME || core.getInput('container_out_host_volume_path');
 
     const instance = got.extend({
         prefixUrl: URL,
